370fb0e5971da297f0ef879614b79d3dd17cf4c0
Gabe Nuñez <gabe@gabenunez.com>
2019-06-12 08:07:54 -0700

    Initial commit of infinite scroll functionality :)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Item from './components/item';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  
+  state = {
+    error: false,
+    isLoading: false,
+    users: [],
+  }
+
+  constructor(props) {
+    super(props);
+
+    window.onscroll = () => {
+      // Checks that the page has scrolled to the bottom
+      if (
+        window.innerHeight + document.documentElement.scrollTop
+        === document.documentElement.offsetHeight
+      ) {
+        this.fetchUsers();
+      }
+    };
+  }
+
+  componentDidMount() {
+    this.fetchUsers();
+  }
+
+  fetchUsers() {
+    this.setState({
+      isLoading: true
+    })
+
+    axios.get('https://randomuser.me/api/?results=50')
+      .then((response) => {
+        // handle success
+        this.setState({
+          isLoading: false
+        })
+
+        this.setState({
+          users: [...this.state.users, ...response.data.results]
+        })
+
+        console.log(this.state.users)
+      })
+      .catch((error) => {
+        console.error(error);
+        this.setState({
+          error: true,
+          isLoading: false
+        })
+      })
+  }
+
+  render() {
+      return (
+        <div className="App">
+          {this.state.users.map((user) => <p key={user.login.uuid}>{user.name.title} {user.name.first} {user.name.last}</p>)}
+          {this.state.isLoading && <h3>Loading...</h3>}
+        </div>
+    );
+  }
 }
 
 export default App;
